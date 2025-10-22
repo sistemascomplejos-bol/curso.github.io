e421e2e64235dac0d474e3a71cdd6cd6cd37f926
sistemascomplejos-bol <sistemascomplejosumsa@gmail.com>
2025-10-21 22:58:44 -0400

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "",
+        fax: "",
+        email: "",
+        address: "",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "d",
+            title: "",
+            subtitle: "",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
